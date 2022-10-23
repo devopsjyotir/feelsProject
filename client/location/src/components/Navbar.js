e8b8c4906d02f71149b60ab295fb4fbc4efb0a26
Jyotir Tyrese McEwanAnamoah <jyotirjy@gmail.com>
2022-10-23 17:57:25 -0400

    added Nav component

---
+++ client/location/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h4></h4>
+      <div>
+        <Link to="/">Home</Link>
+      </div>
+    </nav>
+  );
+};
