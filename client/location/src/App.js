6b6b64c877b4e39309bccb291550c6caedfd8804
Jyotir Tyrese McEwanAnamoah <jyotirjy@gmail.com>
2022-10-24 16:08:14 -0400

    setup route for backend and updated server.js

--- client/location/src/App.js
+++ client/location/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import "./App.css";
 import Form from "./components/Form";
+
 const App = () => {
   return (
     <div className="page">
--- client/location/src/components/Form.jsx
+++ client/location/src/components/Form.jsx
@@ -1,43 +1,98 @@
 import { useState } from "react";
 
-const newForm = () => {
+import bgImg from "../image/zac-gudakov-faBWQt9i7dg-unsplash.jpg";
+const Form = () => {
   const initialState = {
     name: "",
     email: "",
     number: "",
-    interests: [""],
   };
-};
+  const [formState, setFormState] = useState(initialState);
 
-import bgImg from "../image/zac-gudakov-faBWQt9i7dg-unsplash.jpg";
-const Form = () => {
+  const [interestsState, setInterestsState] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+    setFormState(initialState);
+    setInterestsState([]);
+  };
+
+  // const [clicked, SetClicked] = useState(false);
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value });
+  };
+
+  const handleClick = (event) => {
+    event.currentTarget.style.border = "1px solid black";
+
+    // SetClicked(false);
+    setInterestsState([...interestsState, event.target.value]);
+  };
+
   return (
     <section>
       <div className="inputForm">
         <div className="col-1">
           <h2>Find A Home</h2>
           <span>HAve Fun!</span>
-          <form id="form" className="flex flex-col">
-            <input type="text" placeholder="full name" />
-            <input type="text" placeholder="email" />
+          <form id="form" className="flex flex-col" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="full name"
+              id="name"
+              onChange={handleChange}
+              value={formState.name}
+            />
+            <input
+              type="text"
+              placeholder="email"
+              id="email"
+              onChange={handleChange}
+              value={formState.email}
+            />
             <input type="text" placeholder="confirm email" />
-            <input type="text" placeholder="number" />
-            <div className="options">
+            <input
+              type="text"
+              placeholder="number"
+              id="number"
+              onChange={handleChange}
+              value={formState.number}
+            />
+            <div className="options" id="interests">
               <h4>Interests</h4>
-              <button type="button" className="individual-btn" value="running">
+              <button
+                type="button"
+                className="individual-btn"
+                value="running"
+                onClick={handleClick}
+              >
                 running
               </button>
               <button
                 type="button"
                 className="individual-btn"
                 value="cross-fit"
+                onClick={handleClick}
               >
                 cross-fit
               </button>
-              <button type="button" className="individual-btn" value="soccer">
+              <button
+                type="button"
+                className="individual-btn"
+                value="soccer"
+                id="interests"
+                onClick={handleClick}
+              >
                 soccer
               </button>
-              <button type="button" className="individual-btn" value="music">
+              <button
+                type="button"
+                className="individual-btn"
+                value="music"
+                onClick={handleClick}
+              >
                 music
               </button>
             </div>
