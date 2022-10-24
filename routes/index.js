5cf8a77b6541d7b8c03f8ac1b042168a15415888
Jyotir Tyrese McEwanAnamoah <jyotirjy@gmail.com>
2022-10-24 16:26:31 -0400

    implemented a create route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const controllers = require("../controllers");
 const router = Router();
 
 router.get("/", (req, res) => res.send("This is a root"));
 module.exports = router;
+
+router.post("/users", controllers.createUser);
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const db = require("./db");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-const routes = require("./routes");
 
 app.use("/api", routes);
 db.on("error", console.error.bind(console, "MongoDb connection error"));
