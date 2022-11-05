8ebf337f310cefe59b437ac6c129edc9eafe7d11
jyotir <jyotir@pop-os.localdomain>
2022-11-04 20:39:34 -0400

    created update form and made axios call

--- client/location/src/components/Locations.jsx
+++ client/location/src/components/Locations.jsx
@@ -1,10 +1,12 @@
 import axios from "axios"
 import { useEffect } from "react"
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 const Locations = ()=>{
 const [locations, setLocations] = useState([])
+let {id} = useParams()
 
 useEffect(()=>{
     const getLocations = async ()=>{
@@ -15,19 +17,44 @@ useEffect(()=>{
     getLocations()
 }, [])
 
-const updateLocation = (id)=> {
-const newdetails = prompt("Enter New Location")
+const [formState, setFormState] = useState({
+    name:'',
+    image: '',
+    description: '',
+    attributes:[]
+})
+
+const handleChange = (event) => {
+    setFormState({...formState, [event.target.id]: event.target.value})
+}
+
+const updateLocation = async (event)=> {
+event.preventDefault()
+const response = await axios.put(`http://localhost:3001/api/places/update/${id}`, formState)
+setLocations([...locations, response.data])
+// setFormState({})
 }
 
     return(
         <div>
             {locations.map((city)=>(
-                <div>
-                    <p>{city.name}</p>
-                    <button onClick={()=>{
-                        updateLocation(city._id)
-                    }}>Update</button>
-                </div>
+                <form onSubmit={updateLocation} >
+                     <label htmlFor="name">Name: </label>
+                <input id="name" value={formState.name} onChange={handleChange} />
+                <label htmlFor="image">Image: </label>
+                <input id="image" value={formState.image} onChange={handleChange} />
+                <label htmlFor="description">Description: </label>
+                <input
+                  id="description"
+                  value={formState.description}
+                  onChange={handleChange}
+                />
+                <label htmlFor="attributes">Link: </label>
+                <input id="attributes" value={formState.attributes} onChange={handleChange} />
+               
+               
+                <button type="submit">update Snail</button>
+              </form>
             ))}
         </div>
     )
